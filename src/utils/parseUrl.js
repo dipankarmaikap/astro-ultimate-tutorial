912eacd9c180e413bcec751c8ee7f80f697a8878
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2023-10-18 20:17:20 +0530

    Bug fix

--- src/utils/parseUrl.js
+++ src/utils/parseUrl.js
@@ -1,16 +1,15 @@
 import { languages } from './langs'
 
 export default function parseUrl(url) {
-  if (!url) {
-    return { slug: url, language: 'en', langSwitch: { en: '/', es: '/es/' } }
-  }
   let urlToArray = url?.split('/')
   let defaultLang = 'en'
   let isKnownLang = languages.some((l) => l === urlToArray?.[0])
-  let currentLang = isKnownLang ? urlToArray[0] : defaultLang
-  let slug = isKnownLang
-    ? urlToArray?.slice(1)?.join('/') || undefined
-    : urlToArray?.join('/')
+  let currentLang = url && isKnownLang ? urlToArray[0] : defaultLang
+  let slug = url
+    ? isKnownLang
+      ? urlToArray?.slice(1)?.join('/') || undefined
+      : urlToArray?.join('/')
+    : undefined
   let langSwitch = {}
   languages.forEach((lang) => {
     langSwitch = {
